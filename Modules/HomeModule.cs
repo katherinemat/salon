fe23fa453d868456bebc491a1c4829e949f0675c
Katherine <katherinematthews.polsci@gmail.com>
2017-02-24 12:38:13 -0800

    add home module and view for homepage.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+
+namespace Salon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
