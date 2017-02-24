52bf4fafe92f0e0d7a834e45864520b63fbba059
Katherine <katherinematthews.polsci@gmail.com>
2017-02-24 13:00:06 -0800

    add views to see all clients and to add new clients.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,21 @@ namespace Salon
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/clients"] = _ => {
+        List<Client> allClients = Client.GetAll();
+        return View["clients.cshtml", allClients];
+      };
+      Get["/stylists"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["stylists.cshtml", allStylists];
+      };
+      Get["/clients/form"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["clients_form.cshtml", allStylists];
+      };
+      Get["/stylists/form"] = _ => {
+        return View["stylists_form.cshtml"];
+      };
     }
   }
 }
