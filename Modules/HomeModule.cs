af974abd8c905526c9a1b361b1f6a6310c8c0575
Katherine <katherinematthews.polsci@gmail.com>
2017-02-24 17:00:15 -0800

    add patch route to update client information.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -63,22 +63,20 @@ namespace Salon
         model.Add("stylists", allStylists);
         return View["client_edit.cshtml", model];
       };
-      // Patch["client/edit/{id}"] = parameters => {
-      //   Client foundClient = Client.Find(parameters.id);
-      //   foundClient.UpdateName(Request.Form["client-name"]);
-      //   foundClient.UpdateNotes(Request.Form["client-notes"]);
-      //   foundClient.UpdateStylistId(Request.Form["client-stylist"]);
-      //   Dictionary<string, object> model = new Dictionary<string, object>{};
-      //   var updatedClient = Client.Find(parameters.id);
-      //   var foundStylist = Stylist.Find(foundClient.GetStylistId());
-      //   var allStylists = Stylist.GetAll();
-      //   var clientReviews = foundClient.GetClientReview();
-      //   model.Add("cuisines", allStylists);
-      //   model.Add("cuisine", foundStylist);
-      //   model.Add("client", updatedClient);
-      //   model.Add("reviews", clientReviews);
-      //   return View["client_edit.cshtml", model];
-      // };
+      Patch["client/edit/{id}"] = parameters => {
+        Client foundClient = Client.Find(parameters.id);
+        foundClient.UpdateName(Request.Form["client-name"]);
+        foundClient.UpdateNotes(Request.Form["client-notes"]);
+        foundClient.UpdateStylistId(Request.Form["client-stylist"]);
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        var updatedClient = Client.Find(parameters.id);
+        model.Add("client", updatedClient);
+        var foundStylist = Stylist.Find(updatedClient.GetStylistId());
+        model.Add("stylist", foundStylist);
+        var allStylists = Stylist.GetAll();
+        model.Add("stylists", allStylists);
+        return View["client_edit.cshtml", model];
+      };
     }
   }
 }
